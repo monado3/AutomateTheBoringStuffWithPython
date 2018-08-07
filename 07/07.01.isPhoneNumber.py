1bfb6cfd48eee039ada7458e50c401565797633a
monado3 <monade.kk@gmail.com>
2018-08-08 01:32:04 +0900

    now at 7.6

--- 07/07.01.isPhoneNumber.py
+++ 07/07.01.isPhoneNumber.py
@@ -1,3 +1,30 @@
-def is_phone_number(text):
-   if len(text) != 12:
-       return False
+def isPhoneNumber(text: str) -> bool:
+    if len(text) != 12:
+        return False
+    for i in range(3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+
+print('415-555-4242 is a phone number:')
+print(isPhoneNumber('415-555-4242'))
+print('Moshi moshi is a phone number:')
+print(isPhoneNumber('Moshi moshi'))
+
+message = '明日415-555-1011に電話してください。オフィスは415-555-9999です。'
+for i in range(len(message)):
+    chunk = message[i:i + 12]
+    if isPhoneNumber(chunk):
+        print('found phone number: ' + chunk)
+print('done')
